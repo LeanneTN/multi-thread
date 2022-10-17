f1aa4856946b7d4dfcbf8557187a649ea3662b45
LeanneTN <2321604632@qq.com>
2022-10-17 10:19:20 +0800

    structure of project

--- src/MyThread.java
+++ src/MyThread.java
@@ -1,2 +1,18 @@
-public class MyThread {
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+
+public class MyThread implements Runnable{
+    private CountDownLatch latch = null;
+    private String name;
+
+    public MyThread(CountDownLatch latch ,String name){
+        this.latch = latch;
+        this.name = name;
+    }
+    @Override
+    public void run() {
+
+        latch.countDown();
+    }
+
 }
